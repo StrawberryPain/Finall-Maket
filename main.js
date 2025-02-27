07776413085240dbae2f016c675a1e66a46195d2
Anastasiya Yakovenko <yakovenkonastya0@gmail.com>
2025-02-27 09:14:25 +0300

    Last Commit

--- main.js
+++ main.js
@@ -1,31 +1,136 @@
-'use strict';
+"use strict";
+import "/styles/index.scss";
+
+const swiperOptions = {
+  loop: true,
+  slidesPerView: "auto",
+  spaceBetween: 16,
+
+  pagination: {
+    el: ".swiper-pagination",
+  },
+};
 
 window.onload = () => {
-    const swiper = new Swiper(".swiper", {
-      loop: true,
-      slidesPerView: "auto",
-      spaceBetween: 16,
-  
-      pagination: {
-        el: ".swiper-pagination",
-      },
-    });
-  };
-  
-  const toggleClassName = (element, className) => {
-    const isActive = element.classList.contains(className);
-  
-    if (isActive) {
-      element.classList.remove(className);
-    } else {
-      element.classList.add(className);
-    }
-  };
-  
-  const buttonExpand = document.querySelector(".button-expand");
-  const menu = document.querySelector(".menu");
-  
-  buttonExpand.addEventListener("click", () => {
-    toggleClassName(buttonExpand, "expanded");
-    toggleClassName(menu, "expanded");
-  });
+  const swiperBrands = new Swiper(".swiper-brands", swiperOptions);
+  const swiperRepair = new Swiper(".container-repair-swiper", swiperOptions);
+  const swiperServices = new Swiper(".services-swiper", swiperOptions);
+};
+
+const toggleClassName = (element, className) => {
+  const isActive = element.classList.contains(className);
+
+  if (isActive) {
+    element.classList.remove(className);
+  } else {
+    element.classList.add(className);
+  }
+};
+
+const removeClassName = (element, className) => {
+  element.classList.remove(className);
+};
+
+const addClassName = (element, className) => {
+  element.classList.add(className);
+};
+
+const buttonExpand = document.querySelector(".button-expand");
+const menu = document.querySelector(".menu");
+
+buttonExpand.addEventListener("click", () => {
+  toggleClassName(buttonExpand, "expanded");
+  toggleClassName(menu, "expanded");
+});
+
+const repairToggle = document.querySelector('.container-repair__toggle');
+const repairContainer = document.querySelector('.container-repair');
+
+repairToggle.addEventListener("click", () => {
+  toggleClassName(repairToggle, "expanded");
+  toggleClassName(repairContainer, "expanded");
+});
+
+const overlay = document.querySelector(".overlay");
+
+const menuOpen = document.getElementById("menu-open");
+const menuClose = document.getElementById("menu-close");
+const menuFrame = document.getElementById("menu");
+
+menuOpen.onclick = () => {
+  toggleClassName(overlay, "opened");
+  toggleClassName(menuFrame, "opened");
+};
+
+menuClose.onclick = () => {
+  toggleClassName(overlay, "opened");
+  toggleClassName(menuFrame, "opened");
+};
+
+const callOpen = document.getElementById("call-open");
+const callOpenHeader = document.getElementById("call-open-header");
+const callClose = document.getElementById("call-close");
+const callFrame = document.getElementById("call-frame");
+
+callOpen.onclick = () => {
+  addClassName(overlay, "opened");
+  addClassName(callFrame, "opened");
+
+  removeClassName(menuFrame, "opened");
+};
+
+callOpenHeader.onclick = () => {
+  addClassName(overlay, "opened");
+  addClassName(callFrame, "opened");
+
+  removeClassName(menuFrame, "opened");
+};
+
+callClose.onclick = () => {
+  removeClassName(overlay, "opened");
+  removeClassName(callFrame, "opened");
+
+  removeClassName(menuFrame, "opened");
+};
+
+const feedbackOpen = document.getElementById("feedback-open");
+const feedbackOpenHeader = document.getElementById("feedback-open-header");
+const feedbackClose = document.getElementById("feedback-close");
+const feedbackFrame = document.getElementById("feedback-frame");
+
+feedbackOpen.onclick = () => {
+  addClassName(overlay, "opened");
+  addClassName(feedbackFrame, "opened");
+
+  removeClassName(menuFrame, "opened");
+};
+
+feedbackOpenHeader.onclick = () => {
+  addClassName(overlay, "opened");
+  addClassName(feedbackFrame, "opened");
+
+  removeClassName(menuFrame, "opened");
+};
+
+feedbackClose.onclick = () => {
+  removeClassName(overlay, "opened");
+  removeClassName(feedbackFrame, "opened");
+
+  removeClassName(menuFrame, "opened");
+};
+
+overlay.onclick = () => {
+  removeClassName(overlay, "opened");
+
+  removeClassName(menuFrame, "opened");
+  removeClassName(feedbackFrame, "opened");
+  removeClassName(callFrame, "opened");
+}
+
+const informationHiddenText = document.getElementById("information-hidden-text");
+const informationTextButton = document.getElementById("information-text-toggle");
+
+informationTextButton.onclick = () => {
+  toggleClassName(informationHiddenText, "opened");
+  toggleClassName(informationTextButton, "opened");
+};
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  entry: "./main.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff2?|ttf|eot|otf)$/,
+        type: "asset/resource",
+        generator: {
+          filename: "fonts/[name][ext]",
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "styles/index.css",
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: "images", to: "images" },
+      ],
+    }),
+  ],
+  devServer: {
+    static: path.resolve(__dirname, "dist"),
+    port: 3000,
+    open: true,
+  },
+  mode: "development",
+};
